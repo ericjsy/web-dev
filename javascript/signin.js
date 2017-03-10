f6c517622bd619bfe7d1b4759836abc6c7954cd5
ericjsy <e.sy@oceans.ubc.ca>
2017-03-09 18:54:56 -0800

    Added individual warnings to each field that fails warning

--- javascript/signin.js
+++ javascript/signin.js
@@ -11,21 +11,20 @@ function $(id){
 /* Sign Up Form */
 
 function validLogin(){
-	var loginInfo = [$('txtUsername'), $('txtPassword')];
-	var errorMessage = ["username", "password"];
+	var loginInfo = ['txtUsername', 'txtPassword'];
 	
 	// Resets all borders
 	for ( var j = 0; j < loginInfo.length; j++ ) {
-		loginInfo[j].style.border = "";	
+		$(loginInfo[j]).style.border = "";	
 	}
 	
-	// Indicates missing field and prints out appropriate error message
+	// Indicates missing field and prints out an error message
 	for ( var i = 0; i < loginInfo.length; i++ ) {
 
-		if ( loginInfo[i].value.length < 1 ) {
-			$('loginError').innerHTML = "Please provide your " + errorMessage[i] + ".";
-			loginInfo[i].style.border = "1px solid red";
-			loginInfo[i].focus();
+		if ( $(loginInfo[i]).value.length < 1 ) {
+			$('errLogin').innerHTML = "Please fill in all required fields.";
+			$(loginInfo[i]).style.border = "1px solid red";
+			$(loginInfo[i]).focus();
 		return false;
 		}	
 		
@@ -45,12 +44,11 @@ function testMatchingPassword(id){
 
 function verifyPassword(id){
 	$(id).style.border = "";
-	$('signUpError').innerHTML = "";
+	$('errVerify').innerHTML = "";
 
 	if ( !testMatchingPassword(id) ) {
-		$('signUpError').innerHTML = "You must re-enter the same password!";
+		$('errVerify').innerHTML = "You must re-enter the same password!";
 		$(id).style.border = "1px solid red";
-		$(id).focus();
 	}
 }
 
@@ -59,6 +57,7 @@ function testValidEmail(id){
 	var str = $(id).value;
 	
 	return ( str.length > 0 
+		&& (/[a-zA-Z\d_]+\@[a-zA-z]+\.[a-zA-z]{2,3}/.test( $(id).value ))
 		&& ( str.substring(str.length-4) == ".com" 
 		|| str.substring(str.length-4) == ".org" 
 		|| str.substring(str.length-3) == ".ca" ) );
@@ -66,51 +65,49 @@ function testValidEmail(id){
 
 function warnInvalidEmail(id){
 	$(id).style.border = "";
-	$('signUpError').innerHTML = "";
+	$('errEmail').innerHTML = "";
 	
 	if ( !testValidEmail(id) ){
-		$('signUpError').innerHTML = " Please provide an email ending in .com, .ca, or .org";
+		$('errEmail').innerHTML = "Valid emails require an '@' and must end in .com, .ca, or .org";
 		$(id).style.border = "1px solid red";
-		$(id).focus();
 	}
 }
 
 // Test submission
 function validSignup() {
 	var signUpInfo = ['txtNewUser', 'txtNewPassword', 'txtVerify', 'txtEmail'];
-	var errorMessage = ["provide a username", "provide a password", "verify your password", "provide an email"];
 	
 	// Resets all borders
 	for ( var j = 0; j < signUpInfo.length; j++ ) {
 		$(signUpInfo[j]).style.border = "";	
 	}
 	
-	for (var i = 0; i < signUpInfo.length; i++) {
+	for ( var i = 0; i < signUpInfo.length; i++ ) {
 
 		// Indicates missing field and prints out appropriate error message
 		if ( $(signUpInfo[i]).value.length < 1 ) {
-			$('signUpError').innerHTML = "Please " + errorMessage[i] + ".";
+			$('errSignUp').innerHTML = "Please fill in all required fields.";
 			$(signUpInfo[i]).style.border = "1px solid red";
 			$(signUpInfo[i]).focus();
 			return false;
 		}
 		
-		if ( i == 2){
+		if ( i == 2 ) {
 			
 			// Tests for verified password
-			if ( !testMatchingPassword(signUpInfo[i]) ){
-				$('signUpError').innerHTML = " You must re-enter the same password!";
+			if ( !testMatchingPassword(signUpInfo[i]) ) {
+				$('errVerify').innerHTML = " You must re-enter the same password!";
 				$(signUpInfo[i]).style.border = "1px solid red";
 				$(signUpInfo[i]).focus();
 				return false;
 			}
 		}
 		
-		if ( i == 3 ){
+		if ( i == 3 ) {
 			
 			// Tests for a valid email
-			if ( !testValidEmail(signUpInfo[i]) ){
-				$('signUpError').innerHTML = " Valid emails contain an @ symbol and end with .com, .ca, or .org.";
+			if ( !testValidEmail(signUpInfo[i]) ) {
+				$('errEmail').innerHTML = " Valid emails contain an @ symbol and end with .com, .ca, or .org.";
 				$(signUpInfo[i]).style.border = "1px solid red";
 				$(signUpInfo[i]).focus();
 				return false;
