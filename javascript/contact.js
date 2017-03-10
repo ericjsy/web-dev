4d758606695703fde8c78e3f73c5c96f1606fc4f
ericjsy <e.sy@oceans.ubc.ca>
2017-03-09 17:30:21 -0800

    Added individual warnings to each field that fails warning. Added some pattern validation.

--- javascript/contact.js
+++ javascript/contact.js
@@ -10,40 +10,60 @@ function $(id){
 
 /* Contact Us Form */
 
+// Validate name
+function warnInvalidName(id){
+	$(id).style.border = "";
+	$('errFullName').innerHTML = ""
+	
+	if ( /[^a-zA-Z ]+/.test( $(id).value ) ) {
+		$(id).style.border = "1px solid red";
+		
+		if ( id == "txtFullName" ){
+			$('errFullName').innerHTML = " Valid names contain only alphabetical characters.";
+		}
+
+	}
+}
+
+// Validate Email
 function testValidEmail(id) {
 	var str = $(id).value;
 	
-	return ( str.length > 0 && str.substring(str.length-4) == ".com" || str.substring(str.length-4) == ".org" || str.substring(str.length-3) == ".ca" );
+	return ( str.length > 0 
+		&& (/[a-zA-Z\d_]+\@[a-zA-z]+\.[a-zA-z]{2,3}/.test( $(id).value ))
+		&& str.substring(str.length-4) == ".com" 
+		|| str.substring(str.length-4) == ".org" 
+		|| str.substring(str.length-3) == ".ca" );
 }
 
 function warnInvalidEmail(id) {
-	if ( !testValidEmail(id) )
-		$('feedbackError').innerHTML = " Valid emails end in .com, .ca, or .org";
+	$(id).style.border = "";
+	$('errEmail').innerHTML = "";
+	
+	if ( !testValidEmail(id) ){
+		$('errEmail').innerHTML = " Valid emails require an '@' and must end in .com, .ca, or .org";
+		$(id).style.border = "1px solid red";
+	}
 }
 
 function validFeedback() {
-	var feedbackInfo = [$('fname'), $('email'), $('feedback')];
-	var errorMessage = ["full name", "email", "feedback"];
+	var feedbackInfo = ['txtFullName', 'txtEmail', 'txtFeedback'];
 		
 	// Resets all borders
 	for ( var j = 0; j < feedbackInfo.length; j++ ) {
-		feedbackInfo[j].style.border = "";	
+		$(feedbackInfo[j]).style.border = "";	
 	}
 	
-	// Indicates missing field and prints out appropriate error message
+	// Indicates missing field and prints out error message
 	for (i = 0; i < feedbackInfo.length; i++) {
 		
-		if ( feedbackInfo[i].value.length < 1 ) {
-			$('feedbackError').innerHTML = " Please provide your " + errorMessage[i] + ".";
-			feedbackInfo[i].style.border = "1px solid red";
-			feedbackInfo[i].focus();
+		if ( $(feedbackInfo[i]).value.length < 1 ) {
+			$('errFeedback').innerHTML = " Please fill in all required fields.";
+			$(feedbackInfo[i]).style.border = "1px solid red";
+			$(feedbackInfo[i]).focus();
 			return false;
 		}	
 		
-	}
-	
-	if ( !testValidEmail(id) ) {
-		return false;
 	}
 	
 }
