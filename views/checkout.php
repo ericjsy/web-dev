a6a2ae1e4f3d834624824a6b00c70f49f2324e11
Ryalia888 <ryalia888@gmail.com>
2017-04-06 20:19:53 -0700

    All php and css validated, sizing issues fixed

--- views/checkout.php
+++ views/checkout.php
@@ -13,6 +13,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Madeleine's</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
 	<link rel="stylesheet" href="../style/base.css" media="screen">
 	<link rel="stylesheet" href="../style/checkout.css" media="screen">
 	<link rel="stylesheet" href="../style/printbase.css" media="print">
--- views/products.php
+++ views/products.php
@@ -14,6 +14,7 @@
 	<meta charset="utf-8">
 	<title>Madeleine's</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
 	<link rel="stylesheet" href="../style/products.css">
 	<link rel="stylesheet" href="../style/printbase.css" media="print">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
